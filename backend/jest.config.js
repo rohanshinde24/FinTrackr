25d0d64d3f958d132f83da1b9c894c6d76673dbc
rohanshinde24 <iamrohan98764321@gmail.com>
2026-09-06 22:22:45 -0700

    test(api): replace mock suites with integration tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -23,9 +23,7 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
   verbose: true,
-  forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
 };
-
